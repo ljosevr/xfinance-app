86aad00a094ce69403876c62e96bd120ae6afc13
LuisJose <ljosevr@gmail.com>
2021-06-15 16:24:40 -0500

    Mejora de consultas

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -1,12 +1,14 @@
---select * from empresas;
---select * from productos where empresa_id = 'a5a53323-a969-4d0c-ade4-1ff0764c42d7';
+select * from empresas;
+select * from productos where empresa_id = '81a4454c-ab08-41a1-ae38-f96fd8a66453';
 
 select p.id, p.nombre_producto, a.cantidad, c.cantidad from inventario_actual a, inventario_actual_costo c, productos p
-where a.empresa_id = 'a5a53323-a969-4d0c-ade4-1ff0764c42d7'
-AND c.empresa_id = 'a5a53323-a969-4d0c-ade4-1ff0764c42d7'
+where a.empresa_id = '81a4454c-ab08-41a1-ae38-f96fd8a66453'
+AND c.empresa_id = '81a4454c-ab08-41a1-ae38-f96fd8a66453'
+AND p.empresa_id = '81a4454c-ab08-41a1-ae38-f96fd8a66453'
+AND p.id = a.producto_id
 AND c.producto_id = a.producto_id
 AND c.cantidad <> a.cantidad
 AND c.activo;
 
-select * from inventario_actual where producto_id = '6295e79e-23fe-4e8a-bb67-0c6a31157930';
-select * from inventario_actual_costo where producto_id = '6295e79e-23fe-4e8a-bb67-0c6a31157930';
+select * from inventario_actual where producto_id = '81a4454c-ab08-41a1-ae38-f96fd8a66453';
+select * from inventario_actual_costo where producto_id = '81a4454c-ab08-41a1-ae38-f96fd8a66453';
